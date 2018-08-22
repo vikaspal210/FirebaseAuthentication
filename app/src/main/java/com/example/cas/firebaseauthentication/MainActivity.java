a07530ceb9ae46c133905d107d18ff2f693db2df
Vikas Pal <pal.vikas210@gmail.com>
2018-08-22 20:58:22 +0530

    Check current auth state

--- app/src/main/java/com/example/cas/firebaseauthentication/MainActivity.java
+++ app/src/main/java/com/example/cas/firebaseauthentication/MainActivity.java
@@ -3,11 +3,31 @@ package com.example.cas.firebaseauthentication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+    //declare instance of FirebaseAuth
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //initialize FirebaseAuth instance
+        mAuth=FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser currentLoggedUser){
+        /* display Email and Name using currentLoggedUser.getEmail();
+        currentLoggedUser.getDisplayName();*/
     }
 }
