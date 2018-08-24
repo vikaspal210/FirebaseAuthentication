6da8922cb3b5923ee79d9fe8548c05956baed5a7
Vikas Pal <pal.vikas210@gmail.com>
2018-08-24 23:42:22 +0530

    Added updateUI code

--- app/src/main/java/com/example/cas/firebaseauthentication/MainActivity.java
+++ app/src/main/java/com/example/cas/firebaseauthentication/MainActivity.java
@@ -2,18 +2,34 @@ package com.example.cas.firebaseauthentication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener {
+
+    private static final String TAG = "MainActivity";
+    private static final int RC_SIGN_IN = 9001;
     //declare instance of FirebaseAuth
     private FirebaseAuth mAuth;
+    private TextView mStatusTextView;
+    private TextView mDetailTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Views
+        mStatusTextView = findViewById(R.id.status);
+        mDetailTextView = findViewById(R.id.detail);
+
+        // Button listeners
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+        findViewById(R.id.sign_out_button).setOnClickListener(this);
+        findViewById(R.id.disconnect_button).setOnClickListener(this);
         //initialize FirebaseAuth instance
         mAuth=FirebaseAuth.getInstance();
     }
@@ -27,7 +43,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void updateUI(FirebaseUser currentLoggedUser){
-        /* display Email and Name using currentLoggedUser.getEmail();
-        currentLoggedUser.getDisplayName();*/
+        hideProgressDialog();
+        if(currentLoggedUser!=null){
+            mStatusTextView.setText(getString(R.string.google_status_fmt,currentLoggedUser.getEmail()));
+            mDetailTextView.setText(getString(R.string.firebase_status_fmt,currentLoggedUser.getUid()));
+
+            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
+        }else{
+            mStatusTextView.setText("Signed Out");
+            mDetailTextView.setText(null);
+
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
